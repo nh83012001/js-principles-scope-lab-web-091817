2963ba61f724d248565192c02235303157a3b2b8
Nick Hall <nickhall122@gmail.com>
2017-10-29 23:25:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = `bob`
+const leastFavoriteCustomer = 'carolyn commons'
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'nickhall'
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'Nick Hall'
+  let favoriteCustomer = 'Nick Hall'
+}
